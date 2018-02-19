68f9fd8c7abcacc05eec9cddbca0618d5de552c9
hatobus <s1240056@gmail.com>
2018-02-19 11:28:11 +0900

    update some error

--- LVEZ_ESP.cpp
+++ LVEZ_ESP.cpp
@@ -5,7 +5,7 @@
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
 #else
-#include "WProgram.h"
+//#include "WProgram.h"
 #endif
 
 #include "LVEZ_ESP.h"
@@ -14,43 +14,43 @@
 
 using namespace std;
 
-LVEZ_ESP8266::LVEZ_ESP8266() {
+LVEZ_8266::LVEZ_ESP8266::LVEZ_ESP8266() {
     string pin_num_analog = "A0";
 }
 
-LVEZ_ESP8266::LVEZ_ESP8266(int pin_number) {
-    int pin_num_digital = pin_number;
+LVEZ_8266::LVEZ_ESP8266::LVEZ_ESP8266(short pin_number) {
+    short pin_num_digital = pin_number;
     pinMode(pin_number, INPUT);
 }
 
-long LVEZ_ESP8266::get_raw_data_analog() {
+long LVEZ_8266:: LVEZ_ESP8266::get_raw_data_analog() {
     long raw_data = 0;
     raw_data = analogRead(pin_num_analog);
     raw_data = raw_data >> 2;
     return raw_data;
 }
 
-long LVEZ_ESP8266::analog_dist_mm() {
+long LVEZ_8266::LVEZ_ESP8266::analog_dist_mm() {
     long distance_mm = 0, raw_data = 0;
     raw_data = get_raw_data_analog();
     distance_mm = raw_data * 5;
     return distance_mm;
 }
 
-long LVEZ_ESP8266::analog_dist_inch() {
+long LVEZ_8266::LVEZ_ESP8266::analog_dist_inch() {
     long distance_inch = 0, raw_data = 0;
     raw_data = get_raw_data_analog();
     distance_inch = (raw_data * 5) / 25.4;
     return distance_inch;
 }
 
-double LVEZ_ESP8266::pulse_dist_mm() {
+double LVEZ_8266::LVEZ_ESP8266::pulse_dist_mm() {
     double distance_mm = 0;
     distance_mm = pulseIn(pin_num_digital, HIGH);
     return distance_mm;
 }
 
-double LVEZ_ESP8266::pulse_dist_inch() {
+double LVEZ_8266::LVEZ_ESP8266::pulse_dist_inch() {
     double distance_inch = 0;
     distance_inch = pulseIn(pin_num_digital, HIGH);
     distance_inch = distance_inch / 25.4;
@@ -58,8 +58,9 @@ double LVEZ_ESP8266::pulse_dist_inch() {
 }
 
 
-LVEZ_ESP32::LVEZ_ESP32(int pin_number) {
+LVEZ_ESP32::LVEZ_ESP32(short pin_number) {
     pin_num = pin_number;
+    pinMode(pin_num, INPUT);
 }
 
 long LVEZ_ESP32::analog_dist_mm() {
@@ -98,11 +99,12 @@ double LVEZ_ESP32::pulse_dist_inch() {
 
 LVEZ_Arduino::LVEZ_Arduino(short pin_number) {
     pin_num = pin_number;
+    pinMode(pin_num, OUTPUT);
 }
 
-long LVEZ_ long LVEZ_Arduino::get_raw_data_analog(string analog_pin) {
+long LVEZ_Arduino::get_raw_data_analog(string analog_pin) {
     long raw_data;
-    raw_data = analogRead(pin_num);
+    raw_data = analogRead(analog_pin);
     return raw_data;
 }
 
--- LVEZ_ESP.h
+++ LVEZ_ESP.h
@@ -10,31 +10,35 @@
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
 #else
-#include "WProgram.h"
+//#include "WProgram.h"
 #endif
 
 #include <string>
 
 using namespace std;
 
-class LVEZ_ESP8266{
-public:
-    LVEZ_ESP8266(); // using analog read
-    LVEZ_ESP8266(short pin_number); // using pulse width
+namespace LVEZ_8266 {
+    class LVEZ_ESP8266 {
+    public:
+        LVEZ_ESP8266(); // using analog read
+        LVEZ_ESP8266(short pin_number); // using pulse width
 
-    long get_raw_data_analog();
+        long get_raw_data_analog();
 
-    long analog_dist_mm();
-    long analog_dist_inch();
+        long analog_dist_mm();
 
-    double pulse_dist_mm();
-    double pulse_dist_inch();
+        long analog_dist_inch();
+
+        double pulse_dist_mm();
+
+        double pulse_dist_inch();
 
 
-private:
-    string pin_num_analog;
-    short pin_num_digital;
-};
+    private:
+        string pin_num_analog;
+        short pin_num_digital;
+    };
+}
 
 class LVEZ_ESP32{
 public:
@@ -46,7 +50,7 @@ public:
     long analog_dist_inch();
 
     double pulse_dist_mm();
-    doubledouble pulse_dist_inch();
+    double pulse_dist_inch();
 
 private:
     short pin_num;
